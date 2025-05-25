a13911de287def0cc120e47ddc5ea3b83c9a58e5
unknown <faisaltaha268@gmail.com>
2025-05-24 19:49:24 -0700

    Minor Changes

--- learningStreamlit.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-# st.write("Hello World!")
-# press = st.button("Press ME!") #return false by default anf after pressing it becomes True
-# print(press)
-st.title("Super Simple Title")
-st.markdown("Chal Nikal")
